9e1c5fe41e6158c73d87d5adc46b49929f4e14c2
Tonrike <tornike@hostelpro.com>
2019-07-24 22:03:02 +0400

    fix(start): installed prop-types

---
+++ src/view/components/sections/sections.js
@@ -0,0 +1,15 @@
+import React     from "react";
+import PropTypes from "prop-types";
+import "./styles.scss";
+
+const sections = props => {
+	return (
+		<div className = "sections">
+
+		</div>
+	);
+};
+
+sections.propTypes = {};
+
+export default sections;
