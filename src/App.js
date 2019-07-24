1e9068743b76f62d090290b69d833ae6ac150ac6
Tonrike <tornike@hostelpro.com>
2019-07-24 17:31:43 +0400

    chore(setup): added fonts and icons

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+	return (
+		<div className = "App">
+		</div>
+	);
 }
 
 export default App;
